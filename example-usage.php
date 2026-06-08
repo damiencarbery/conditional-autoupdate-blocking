843e073ac431db91c332405269637e3d7ccdd312
Damien Carbery <daymobrew@yahoo.com>
2026-06-08 20:48:24 +0100

    Remove error_log() calls.

--- example-usage.php
+++ example-usage.php
@@ -5,7 +5,7 @@ Plugin URI: https://www.damiencarbery.com/
 Description: Examples using the ConditionalAutoUpdateBlocking code to blocking the auto-update of a plugin e.g. limit days one can be updated or any condition you can think of.
 Author: Damien Carbery
 Author URI: https://www.damiencarbery.com
-Version: 0.1.20260608
+Version: 0.2.20260608
 License: GPL v3 or later
 License URI: https://www.gnu.org/licenses/gpl-3.0.html
 */
@@ -54,29 +54,25 @@ function dcwd_no_zero_version_woocommerce( $update, $item ) {
 add_filter( 'should_update_check', 'dcwd_woocommerce_two_weeks_old', 5, 2 );
 function dcwd_woocommerce_two_weeks_old( $update, $item ) {
 	if ( 'woocommerce' == $item->slug ) {
-error_log( 'api.wordpress.org query for woocommerce.' );
 		$url = 'https://api.wordpress.org/plugins/info/1.2/?action=plugin_information&slug=' . $item->slug;
 
 		$res = wp_remote_get( $url );
 		if ( true === is_wp_error( $res ) ) {
-			error_log( 'Wordpress.org API query error: ' . $res->get_error_message() );
+			return $update;
 		}
 
 		$body = wp_remote_retrieve_body( $res );
 		$body = json_decode( $body, true );
 
 		if ( JSON_ERROR_NONE !== json_last_error() ) {
-			error_log( 'Json parsing error: ' . json_last_error_msg() );
 			return $update;
 		}
 
 		$last_updated_age = time() - strtotime( $body['last_updated'] );
 		if ( $last_updated_age > (2 * WEEK_IN_SECONDS) ) {
-			error_log( 'WooCommerce release is older than 2 weeks so can update.' );
 			return $update;
 		}
 		else {
-			error_log( 'WooCommerce release is less than two weeks old so do not update.' );
 			global $ConditionalAutoUpdateBlocking;
 			$ConditionalAutoUpdateBlocking->add_to_email_content( 'WooCommerce update is less than two weeks old so do not update. Will wait until it is two weeks old.' );
 			return false;
