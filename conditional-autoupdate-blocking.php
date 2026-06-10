d50460569be3b6d678d539033721185b549e32fb
Damien Carbery <daymobrew@yahoo.com>
2026-06-10 15:46:57 +0100

    Use function, CAUB(), to return an instance of the class; use this to initialise the class. This is how WooCommerce does it.

--- conditional-autoupdate-blocking.php
+++ conditional-autoupdate-blocking.php
@@ -5,7 +5,7 @@ Plugin URI: https://www.damiencarbery.com/
 Description: Allow for conditionally blocking the auto-update of a plugin e.g. limit days one can be updated or any condition you can think of.
 Author: Damien Carbery
 Author URI: https://www.damiencarbery.com
-Version: 0.2.20260608
+Version: 0.3.20260610
 License: GPL v3 or later
 License URI: https://www.gnu.org/licenses/gpl-3.0.html
 */
@@ -15,11 +15,15 @@ defined( 'ABSPATH' ) || exit;
 
 
 class ConditionalAutoUpdateBlocking {
+	// The single instance of the class.
+	public static $instance = null;
+
+	// The content of the email sent to the admin.
 	private $email_content;
 
 
 	// Returns an instance of this class. 
-	public static function get_instance() {
+	public static function instance() {
 		if ( null == self::$instance ) {
 			self::$instance = new self;
 		} 
@@ -42,7 +46,6 @@ class ConditionalAutoUpdateBlocking {
 		add_action( 'wp_maybe_auto_update', array( $this, 'add_auto_update_filter' ), 5 );
 		// Send email if any updates were blocked.
 		add_action( 'wp_maybe_auto_update', array( $this, 'send_info_about_blocked_updates' ), 100 );
-
 	}
 
 
@@ -53,7 +56,6 @@ class ConditionalAutoUpdateBlocking {
 
 
 	public function should_update_check( $update, $item ) {
-		//error_log( 'Checking whether to update: ' . $item->slug );
 		// Note: false==is_admin() and true==wp_doing_cron(), if such checks are needed.
 
 		return apply_filters( 'should_update_check', $update, $item );
@@ -70,11 +72,19 @@ class ConditionalAutoUpdateBlocking {
 	// Send email if any updates were blocked.
 	public function send_info_about_blocked_updates( $update_results ) {
 		if ( !empty( $this->email_content ) ) {
-			$to = get_site_option( 'admin_email' );
+			$to = get_site_option( 'admin_email' );  // ToDo: Filter the $to value.
 			$subject = 'Some plugin auto-updates blocked ';
 			$body = sprintf( 'Some plugin auto-updates on your site at %s have been blocked. The plugins and reasons are listed below:%s* %s', home_url(), "\n\n", implode( "\n* ", $this->email_content ) );
 			wp_mail( $to, $subject, $body );
 		}
 	}
 }
-$ConditionalAutoUpdateBlocking = new ConditionalAutoUpdateBlocking();
+
+
+// Returns the main instance of the ConditionalAutoUpdateBlocking class.
+function CAUB() { // phpcs:ignore WordPress.NamingConventions.ValidFunctionName.FunctionNameInvalid
+	return ConditionalAutoUpdateBlocking::instance();
+}
+
+
+$ConditionalAutoUpdateBlocking = CAUB();
